2f66df57bcfcd783d455bc44a1c41e4e755f1712
Oziel Veancio <ozieltutoriais@gmail.com>
2026-05-26 18:20:47 -0300

    deploy automatico

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,14 +1,17 @@
-import { initializeApp } from 'firebase/app'
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAvuV2rXnxokLuNtih3X8mowLzYWvfmHWs",
+  apiKey: "AIzaSyAvuV2rXnxokLuNtih3X8mowLzYwvfmHWs",
   authDomain: "autoescolasystem.firebaseapp.com",
   projectId: "autoescolasystem",
   storageBucket: "autoescolasystem.firebasestorage.app",
   messagingSenderId: "1078046723539",
   appId: "1:1078046723539:web:b5c6a5f788ddd10e77f326"
-}
+};
 
-const app = initializeApp(firebaseConfig)
-
-export default app
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
